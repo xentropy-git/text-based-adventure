065f3ad55b0debea7ebf95c913053db664e69e86
xentropy <claponsie@gmail.com>
2024-08-17 12:44:18 -0400

    Added SSO

--- text_based_adventure/text_based_adventure/urls.py
+++ text_based_adventure/text_based_adventure/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from .pages import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path("", views.HomeView.as_view(), name="home"),
     path('storyroom/<int:room_id>/', views.StoryRoomView.as_view(), name='story_room'), 
+    path('', include('django_sso.sso_gateway.urls')),
 ]
